dddca3d22abcd5453f5b0e66198be30fe46fd8d6
Adam Boardman <adamboardman@gmail.com>
2021-01-12 13:58:20 +0000

    Fix typo

--- usr/bin/droid/bind_vendor.sh
+++ usr/bin/droid/bind_vendor.sh
@@ -4,6 +4,6 @@
 #mount -o ro,bind /usr/libexec/droid-hybris/vendor/lib64/hw/audio.primary.mt6771.so /vendor/lib64/hw/audio.primary.mt6771.so
 #mount -o ro,bind /usr/libexec/droid-hybris/vendor/lib64/libmtk-ril.so /vendor/lib64/libmtk-ril.so
 
-#disable secure_element and keymaster as tyey always fail
+#disable secure_element and keymaster as they always fail
 mount -o ro,bind /dev/null /vendor/etc/init/android.hardware.secure_element@1.0-service.rc
 mount -o ro,bind /dev/null /vendor/etc/init/android.hardware.keymaster@3.0-service.rc
