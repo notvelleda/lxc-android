063dfac9d63269912082d527d3411b440cf7cfa3
Adam Boardman <adamboardman@gmail.com>
2021-01-12 15:59:51 +0000

    Stopped some vendor init's from loading invalid or builtin kernel modules

--- usr/bin/droid/bind_vendor.sh
+++ usr/bin/droid/bind_vendor.sh
@@ -7,3 +7,13 @@
 #disable secure_element and keymaster as they always fail
 mount -o ro,bind /dev/null /vendor/etc/init/android.hardware.secure_element@1.0-service.rc
 mount -o ro,bind /dev/null /vendor/etc/init/android.hardware.keymaster@3.0-service.rc
+
+#disable loading of the vendor wlan and bt kernel modules as we include it in our kernel anyway
+mount -o ro,bind /dev/null /vendor/etc/init/init.wlan_drv.rc
+mount -o ro,bind /dev/null /vendor/etc/init/init.bt_drv.rc
+mount -o ro,bind /dev/null /vendor/etc/init/init.wmt_drv.rc
+
+#disable loading of the vendor fmradio and gps drivers as we build and launch our own within gemian
+mount -o ro,bind /dev/null /vendor/etc/init/init.fmradio_drv.rc
+mount -o ro,bind /dev/null /vendor/etc/init/init.gps_drv.rc
+
