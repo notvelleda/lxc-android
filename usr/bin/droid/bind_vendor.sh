a3b2b084f7857f7e6c0ea78420b07ded6c68a29a
Adam Boardman <adamboardman@gmail.com>
2023-04-19 13:07:37 +0100

    Updates for Astro

--- usr/bin/droid/bind_vendor.sh
+++ usr/bin/droid/bind_vendor.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-#patched to reduce cpu usage of mtkfusionrild
-mount -o ro,bind /usr/libexec/droid-hybris/vendor/lib64/libmtk-ril.so /vendor/lib64/libmtk-ril.so
+model=$(</proc/device-tree/model)
+if [[ "$model" =~ ^(.*)MT6771(.*)$ ]];
+then
+  #patched to reduce cpu usage of mtkfusionrild - Cosmo
+  mount -o ro,bind /usr/libexec/droid-hybris/vendor/lib64/libmtk-ril.so /vendor/lib64/libmtk-ril.so
+fi
 
 #disable secure_element and keymaster as they always fail
 mount -o ro,bind /dev/null /vendor/etc/init/android.hardware.secure_element@1.0-service.rc
@@ -15,3 +19,6 @@ mount -o ro,bind /dev/null /vendor/etc/init/init.wmt_drv.rc
 #disable loading of the vendor fmradio and gps drivers as we build and launch our own within gemian
 mount -o ro,bind /dev/null /vendor/etc/init/init.fmradio_drv.rc
 mount -o ro,bind /dev/null /vendor/etc/init/init.gps_drv.rc
+
+#disable health?
+#/vendor/etc/init/android.hardware.health\@2.0-service.rc
